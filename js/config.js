fc3f1474c1f39c02d616cbe3d173b28902f28066
Lee Yen <leeyenwork@gmail.com>
2019-07-24 02:09:17 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_dLy2cT4Sg', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6p6pofo1vqjh7e29dh72ug7uu3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
